5f9f82011e08d5ff638f13c17907bde17e242f63
HARSH KUMAR VIMAL <140744904+hvimal605@users.noreply.github.com>
2025-04-29 02:34:01 +0530

    message.js

--- Backend/models/message.js
+++ Backend/models/message.js
@@ -4,15 +4,15 @@ const mongoose = require("mongoose")
 const messageSchema = new mongoose.Schema({
     senderName:{
         type:String,
-        minLength:[2,"Name must contain at least 2 characters!"]
+     
     },
     subject:{
         type:String,
-        minLength:[2,"Name must contain at least 2 characters!"]
+        
     },
     message:{
         type:String,
-        minLength:[2,"Name must contain at least 2 characters!"]
+       
     },
     email:{
         type:String,
